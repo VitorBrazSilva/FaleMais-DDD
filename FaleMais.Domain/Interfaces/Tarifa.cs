5ec01b10d5b94605a7767b0f95033afc1628937a
Vitor Braz <vitorbraz_@hotmail.com>
2019-05-17 21:50:45 -0300

    Criaçãod a Primeira entidade 'Tarifa'

---
+++ FaleMais.Domain/Interfaces/Tarifa.cs
@@ -0,0 +1,10 @@
+﻿namespace FaleMais.Domain.Interfaces
+{
+    public class Tarifa
+    {
+        public int TarifaId { get; set; }
+        public int origem { get; set; }
+        public int Destino { get; set; }
+        public int ValorMin { get; set; }
+    }
+}
---
+++ FaleMais.Infra.Data/Context/FaleMaisContext.cs
@@ -0,0 +1,16 @@
+﻿using FaleMais.Domain.Interfaces;
+using System.Data.Entity;
+
+namespace FaleMais.Infra.Data.Context
+{
+    public class FaleMaisContext : DbContext        
+    {
+        public FaleMaisContext()
+            : base("FaleMais")
+        {
+
+        }
+
+        public DbSet<Tarifa> Tarifas { get; set; }
+    }
+}
